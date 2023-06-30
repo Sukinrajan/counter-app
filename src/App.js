8e01031b88c52d5a1006da0afce9fba42895fb51
AROKIA SUKINAJAN A <asrajan256@gmail.com>
2023-06-30 16:09:22 +0530

    counter Add sq numbers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(1);
- 
-  
+  let [count, setCount] = useState(0);
+  const [numbers, setNumbers] = useState([]);
+  const [oddNumbers, setOddNumbers] = useState([]);
+  const [evenNumbers, setEvenNumbers] = useState([]);
+  const [sqNumbers, setSqNumbers] = useState([]);
+
+  const updateNumbers = () => {
+    setNumbers([...numbers, count]);
+    setSqNumbers([...sqNumbers, count * count]);
+  };
+
+  const oddNumber = () => {
+    if (count % 2 !== 0) {
+        setOddNumbers([...oddNumbers, count]);
+      }
+  };
+
+  const evenNumber = () => {
+    if (count % 2 === 0) {
+        setEvenNumbers([...evenNumbers, count]);
+      }
+  };
+
+  const incrementCount = () => {
+    count = count + 1;
+    setCount(count);
+    updateNumbers();
+    oddNumber();
+    evenNumber();
+  };
+
+  const decrementCount = () => {
+    if (count > 0) {
+      count = count - 1;
+      setCount(count);
+      updateNumbers();
+      oddNumber();
+    evenNumber();
+    }
+  };
+
+  const resetCount = () => {
+    setCount(0);
+    setNumbers([]);
+    setOddNumbers([]);
+    setEvenNumbers([]);
+    setSqNumbers([]);
+  };
+
   return (
-    <div style={{display:"flex",flexDirection:"column",alignItems:"center"}}>
-      <h1>Counter Appp</h1>
-      <h3>value {count}</h3>
-      <div style={{display:"flex",flexDirection:"row",gap:"50px"}}>
-      
-      <button onClick={()=>setCount(count*5)}> *5 </button>
-      <button onClick={()=>{
-        if(count<5){   //for Don't Show Negative Values
-          setCount(count)
-        }else{
-          setCount(count/5)
-        }
-      } }> /5 </button>
-      <button onClick={()=>setCount(0)}>0</button>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+    
+      <h1>Counter App</h1>
+      <h3>Value: {count}</h3>
+
+      <div style={{ display: "flex", flexDirection: "row", gap: "50px" }}>
+        <button onClick={incrementCount}>+1</button>
+        <button onClick={decrementCount}>-1</button>
+        <button onClick={resetCount}>0</button>
       </div>
+
+      <h4>
+        <div>Number: {numbers.join(", ")}</div>
+        <div>ODD Numbers: {oddNumbers.join(", ")}</div>
+        <div>EVEN Numbers: {evenNumbers.join(", ")}</div>
+        <div>Square Numbers: {sqNumbers.join(", ")}</div>
+      </h4>
     </div>
   );
 }
